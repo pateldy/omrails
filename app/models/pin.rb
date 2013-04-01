bc42c7727b47b559c982381b830b66596d0123ec
drew patel <dypatel88@gmail.com>
2013-03-31 19:57:40 -0500

    Add Pins Associations

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,5 +1,9 @@
-class Pin < ActiveRecord::Base
+ class Pin < ActiveRecord::Base
   attr_accessible :description
 
   validates :description, presence: true
+
+  belongs_to :user
+
+  validates :user_id, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,13 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130329004828) do
+ActiveRecord::Schema.define(:version => 20130401002815) do
 
   create_table "pins", :force => true do |t|
     t.string   "description"
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
+    t.integer  "user_id"
   end
 
   create_table "users", :force => true do |t|
