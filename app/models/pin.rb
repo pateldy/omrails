75953610efd0e6c3eb6dc70c4279fc4281c47285
drew patel <dypatel88@gmail.com>
2013-04-10 20:51:04 -0500

    Add Jquery Masonry

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -7,7 +7,7 @@
   validates_attachement :image, presence: true,
   								content_type: { content_type: ['image/jpeg', 'image/jpg', 'image/png', 'image/gif'] },
   								size: { less_than: 5.megabytes }
-  belongs_to :user_id
-  has_attached_file :image
+  belongs_to :user
+  has_attached_file :image, styles: { medium: "320x240>"}
   
 end
